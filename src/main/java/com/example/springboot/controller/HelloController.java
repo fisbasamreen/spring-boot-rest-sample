2232a38844a138d863bcb8f4fec4d95f46ec3f28
FisbaSamreen <fisbasamreen07@gmail.com>
2023-09-23 00:15:55 +0400

    Added DB and tested with postman

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class HelloController {
     @RequestMapping("/")
     public String home(){
         return "Hello World!";
